87edd0d68f229890377aff16e4cfdc87e165cef0
Laurent <Laurent>
2016-06-07 22:31:40 +0200

    f

--- app.js
+++ app.js
@@ -1,16 +1,95 @@
-function updateContactList(){
-  cozysdk.defineRequest('Contact', 'all', 'function(doc){ emit(doc.n); }', function(err, res) {
-    if (err != null) return alert(err);
-    cozysdk.run('Contact', 'all', {}, function(err, res) {
-      if (err != null) return alert(err);
-      var contacts = JSON.parse(JSON.stringify(res));
-      /* contacts == [
-        {id:"323274828329", key:"Jane;Willson"},
-        {id:"323274827428", key:"John;Smith"}
-      ]*/
-      render(contacts);
-    });
-  });
+function initialize() {
+	document.querySelector('.send').addEventListener('change', onSendChanged);
+	attachEventHandler('table .edit', 'blur', onUpdatePressed);
+	attachEventHandler('table .edit', 'keypress', onUpdateKeyPressed);
+	attachEventHandler('.destroy', 'click', onButtonDestroyClicked);
+	updateContactList();
+}
+
+function onSendChanged() {
+	var contactName = document.querySelector('.send').value;
+
+	if (contactName.trim() === '') {
+		return;
+	}
+
+	var contact = {
+		n: contactName.trim()
+	};
+	cozysdk.create('Contact', contact, function(err, res) {
+		if (err != null) {
+			return alert(err);
+		} else {
+			document.querySelector('.send').value = '';
+			updateContactList();
+		}
+	});
+}
+
+function onUpdatePressed(event) {
+	var contact = {
+		n: event.target.value.trim()
+	};
+
+	cozysdk.updateAttributes('Contact', getIDFromElement(event.target), contact, function(err, res) {
+		if (err) {
+			return alert(err);
+		} else {
+			updateContactList();
+		}
+	});
+}
+
+function onUpdateKeyPressed(event) {
+	if (event.keyCode === 13) {
+		event.target.blur();
+	}
+}
+
+function onButtonDestroyClicked(event) {
+	cozysdk.destroy('Contact', getIDFromElement(event.target), function(err, res) {
+		if (err) {
+			return alert(err);
+		} else {
+			updateContactList();
+		}
+	});
+}
+
+function attachEventHandler(klass, action, listener) {
+	var useCapture = action === 'blur';
+	document.querySelector('.contact-list').addEventListener(action, function(event) {
+		if (event.target.matches(klass)) {
+			listener.call(event.target, event);
+		}
+	}, !!useCapture);
+}
+
+function getIDFromElement(element) {
+	if (element.parentNode.dataset.id) {
+		return element.parentNode.dataset.id;
+	}
+	return getIDFromElement(element.parentNode);
+}
+
+function updateContactList() {
+	cozysdk.defineRequest('Contact', 'all', 'function(doc) { emit(doc.n); }', function(err, res) {
+		if (err != null) {
+			return alert(err);
+		} else {
+			cozysdk.run('Contact', 'all', {}, function(err, res) {
+				if (err != null) {
+					return alert(err);
+				} else {
+					var contacts = JSON.parse("" + res);
+					contacts.forEach(function(contactName) {
+						contactName.key = contactName.key.replace(/ /g, '\u00a0');
+					});
+					render(contacts);
+				}
+			});
+		}
+	});
 }
 
 function render(contacts) {
@@ -27,18 +106,4 @@ function render(contacts) {
 	document.querySelector('.contact-list').innerHTML = HTML;
 }
 
-
-function onSendChanged(){
-  var contactName = document.querySelector('.send').value.trim();
-  var values = {n: contactName};
-  cozysdk.create('Contact', values, function(err, res) {
-    if (err != null) return alert(err);
-    // res.id == "8239283928326"
-    document.querySelector('.send').value = '';
-    updateContactList();
-  });
-}
-
-
-document.querySelector('.send').addEventListener('change', onSendChanged);
-document.addEventListener("DOMContentLoaded", updateContactList);
+document.addEventListener("DOMContentLoaded", initialize);
